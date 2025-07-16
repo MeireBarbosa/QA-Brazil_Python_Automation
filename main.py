db8d4f11991f3625f8bccc3557e0cd76e9b36540
meirelucibarbosa <meireinteriores@hotmail.com>
2025-07-16 17:59:30 +0200

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 
 class TestUrbanRoutes:
+    URBAN_ROUTES_URL = '' # URL do botão para Iniciar o servidor (a definir)
+    ADDRESS_FROM = 'EAST 2nd Street,601'
+    ADDRESS_TO = '1300 1st St'
+    PHONE_NUMBER = '+1 123 123 12 12'
+    CARD_NUMBER = '1234 5678 9100'
+    CARD_CODE = '1111'
+    MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
