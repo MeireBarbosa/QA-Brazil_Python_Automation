54cc30104ddb2d330757dd0a5f2a99001fcfdd4f
meirelucibarbosa <meireinteriores@hotmail.com>
2025-07-18 19:11:43 +0200

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
+import data
+import helpers
+
 
 class TestUrbanRoutes:
-    URBAN_ROUTES_URL = '' # URL do botão para Iniciar o servidor (a definir)
-    ADDRESS_FROM = 'EAST 2nd Street,601'
-    ADDRESS_TO = '1300 1st St'
-    PHONE_NUMBER = '+1 123 123 12 12'
-    CARD_NUMBER = '1234 5678 9100'
-    CARD_CODE = '1111'
-    MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
+
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execuçã0.")
+
+    def test_set_route(self):
+        # Adicionar em S8
+        print("Função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("Função criada para definir o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("Função criada para definir numero de telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("Função criada para preenchimento do cartã0")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("Função criada para inserir comentario ao motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("Função criada para pedir cobertores e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+         print("Função criada para definir o pedido de sorvetes")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("Função criada para escolher modelo do carro")
+        pass
