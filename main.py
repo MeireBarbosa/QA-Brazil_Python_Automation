f7646b4f33f939403a27b818e7d1a298b79515e0
meirelucibarbosa <meireinteriores@hotmail.com>
2025-07-31 18:53:31 +0200

    first commit second commit revisão projeto sp8 seg revisao sp8

--- main.py
+++ main.py
@@ -23,13 +23,16 @@ class TestUrbanRoutes:
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("Conectado ao servidor Urban Routes")
         else:
-            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execuçã0.")
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def teardown_method(self):
+        self.driver.quit()
 
     def test_set_route(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.enter_from_locations(data.ADDRESS_FROM)
-        routes_page.enter_to_locations(data.ADDRESS_TO)
+        routes_page.enter_from_location(data.ADDRESS_FROM)
+        routes_page.enter_to_location(data.ADDRESS_TO)
         assert routes_page.get_from_location_value() == data.ADDRESS_FROM
         assert routes_page.get_to_location_value() == data.ADDRESS_TO
 
@@ -56,7 +59,7 @@ class TestUrbanRoutes:
         routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         routes_page.click_taxi_option()
         routes_page.click_comfort_icon()
-        routes_page.click_add_cartao(data.CARD_NUMBER, data.CARD_CODE)
+        routes_page.click_add_card(data.CARD_NUMBER, data.CARD_CODE)
         assert "Cartão" in routes_page.confirm_card()
 
     def test_comment_for_driver(self):
@@ -65,8 +68,8 @@ class TestUrbanRoutes:
         routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         routes_page.click_taxi_option()
         routes_page.click_comfort_icon()
-        routes_page.add_comentario(data.MESSAGE_FOR_DRIVER)
-        assert data.MESSAGE_FOR_DRIVER in routes_page.coment_confirm()
+        routes_page.add_comment(data.MESSAGE_FOR_DRIVER)
+        assert data.MESSAGE_FOR_DRIVER in routes_page.confirm_comment()
 
     def test_order_blanket_and_handkerchiefs(self):
         self.driver.get(data.URBAN_ROUTES_URL)
@@ -74,8 +77,8 @@ class TestUrbanRoutes:
         routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         routes_page.click_taxi_option()
         routes_page.click_comfort_icon()
-        routes_page.switch_cobertor()
-        assert routes_page.switch_cobertor_active() is True
+        routes_page.toggle_blanket()
+        assert routes_page.toggle_blanket_active() is True
 
     def test_order_2_ice_creams(self):
         self.driver.get(data.URBAN_ROUTES_URL)
@@ -83,11 +86,8 @@ class TestUrbanRoutes:
         routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         routes_page.click_taxi_option()
         routes_page.click_comfort_icon()
-
-        for _ in range(2):
-            routes_page.add_ice()
-
-        assert int(routes_page.qnt_sorvete()) == 2
+        routes_page.order_ice_cream(2)
+        assert int(routes_page.ice_cream_quantity()) == 2
 
     def test_car_search_model_appears(self):
         self.driver.get(data.URBAN_ROUTES_URL)
@@ -96,54 +96,6 @@ class TestUrbanRoutes:
         routes_page.click_taxi_option()
         routes_page.click_comfort_icon()
         routes_page.click_number_text(data.PHONE_NUMBER)
-        routes_page.click_add_cartao(data.CARD_NUMBER, data.CARD_CODE)
-        routes_page.add_comentario(data.MESSAGE_FOR_DRIVER)
-        assert "Buscar carro" in routes_page.pop_up_show()
-
-        def teardown_method(self):
-            self.driver.quit()
-
-
-
-
-    def test_set_route(self):
-        # Adicionar em S8
-        print("Função criada para inserir rota")
-        pass
-
-    def test_select_plan(self):
-        # Adicionar em S8
-        print("Função criada para inserir plano")
-        pass
-
-    def test_fill_phone_number(self):
-        # Adicionar em S8
-        print("Função criada para inserir telefone")
-        pass
-
-    def test_fill_card(self):
-        # Adicionar em S8
-        print("Função criada para inserir cartão")
-        pass
-
-    def test_comment_for_driver(self):
-        # Adicionar em S8
-        print("Função criada para inserir comentario ao motorista")
-        pass
-
-    def test_order_blanket_and_handkerchiefs(self):
-        # Adicionar em S8
-        print("Função criada para pedir cobertores e lenços")
-        pass
-
-    def test_order_2_ice_creams(self):
-        # Adicionar em S8
-        number_of_ice_creams = 2
-        for count in range(number_of_ice_creams):
-         print("Função criada para definir o pedido de sorvetes")
-        pass
-
-    def test_car_search_model_appears(self):
-        # Adicionar em S8
-        print("Função criada para escolher modelo do carro")
-        pass
+        routes_page.click_add_card(data.CARD_NUMBER, data.CARD_CODE)
+        routes_page.add_comment(data.MESSAGE_FOR_DRIVER)
+        assert "Buscar carro" in routes_page.show_popup()
--- pages.py
+++ pages.py
@@ -2,55 +2,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from helpers import retrieve_phone_code
-import time
 
 class UrbanRoutesPage:
-    # Seção "De" e "Para"
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
-
-
-    def enter_phone_number(self, number):
-        phone_input = self.driver.find_element(By.ID, "phone")
-        phone_input.send_keys(number)
-
-    def add_credit_card(self, number, name, expiry, cvv):
-        self.driver.find_element(By.ID, "card_number").send_keys(number)
-        self.driver.find_element(By.ID, "card_name").send_keys(name)
-        self.driver.find_element(By.ID, "card_expiry").send_keys(expiry)
-        cvv_input = self.driver.find_element(By.ID, "code")
-        cvv_input.send_keys(cvv)
-        self.driver.find_element(By.TAG_NAME, "body").click()  # simula perda de foco
-        self.driver.find_element(By.ID, "add_card_button").click()
-
-    def write_driver_comment(self, comment):
-        comment_box = self.driver.find_element(By.ID, "driver_comment")
-        comment_box.send_keys(comment)
-        self.driver.find_element(By.ID, "submit_comment").click()
-
-    def request_blanket_and_wipes(self):
-        blanket_toggle = self.driver.find_element(By.ID, "blanket_toggle")
-        blanket_toggle.click()
-        assert "active" in blanket_toggle.get_attribute("class")
-
-    def order_ice_cream(self, quantity):
-        for _ in range(quantity):
-            self.driver.find_element(By.ID, "add_ice_cream").click()
-
-    def select_tariff_comfort(self):
-        comfort_option = self.driver.find_element(By.ID, "tariff_comfort")
-        if comfort_option.is_displayed():
-            comfort_option.click()
-
-    def send_request_with_message(self, message):
-        message_box = self.driver.find_element(By.ID, "driver_message")
-        message_box.send_keys(message)
-        self.driver.find_element(By.ID, "request_button").click()
-
-
-    # Selecionar tarifa e chamar táxi
+    phone_input = (By.ID, "phone")
+    phone_code_input = (By.ID, "code")
+    card_number_input = (By.ID, "card_number")
+    card_code_input = (By.ID, "card_code")
+    confirm_card_label = (By.ID, "card_confirmed")
+    comment_input = (By.ID, "driver_comment")
+    confirm_comment_label = (By.ID, "comment_submitted")
+    blanket_toggle = (By.ID, "blanket_toggle")
+    ice_cream_button = (By.ID, "add_ice_cream")
+    ice_cream_quantity_label = (By.ID, "ice_cream_count")
+    comfort_tariff = (By.ID, "tariff_comfort")
+    request_button = (By.ID, "request_button")
+    message_input = (By.ID, "driver_message")
+    confirmation_popup = (By.ID, "search_popup")
     taxi_option_locator = (By.XPATH, '//button[contains(text(),"chamar")]')
-    comfort_icon_locator = (By.XPATH, '//img[@src=/static/media/kids.075fd8d4.svg]')
+    comfort_icon_locator = (By.XPATH, '//img[@src="/static/media/kids.075fd8d4.svg"]')
     comfort_active = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]')
 
     def __init__(self, driver):
@@ -75,8 +46,11 @@ class UrbanRoutesPage:
             EC.visibility_of_element_located(self.from_field)
         ).get_attribute('value')
 
+    def get_to_location_value(self):
+        return self.driver.find_element(*self.to_field).get_attribute('value')
+
     def click_taxi_option(self):
-        self.driver.find_element(*self.comfort_icon_locator).click()
+        self.driver.find_element(*self.taxi_option_locator).click()
 
     def click_comfort_icon(self):
         self.driver.find_element(*self.comfort_icon_locator).click()
@@ -90,4 +64,44 @@ class UrbanRoutesPage:
         except Exception:
             return False
 
+    def click_number_text(self, number):
+        self.driver.find_element(*self.phone_input).send_keys(number)
+        code = retrieve_phone_code(self.driver)
+        self.driver.find_element(*self.phone_code_input).send_keys(code)
 
+    def confirm_number(self):
+        return self.driver.find_element(*self.phone_input).get_attribute("value")
+
+    def click_add_card(self, number, cvv):
+        self.driver.find_element(*self.card_number_input).send_keys(number)
+        self.driver.find_element(*self.card_code_input).send_keys(cvv)
+        self.driver.find_element(By.TAG_NAME, "body").click()
+        self.driver.find_element(By.ID, "add_card_button").click()
+
+    def confirm_card(self):
+        return self.driver.find_element(*self.confirm_card_label).text
+
+    def add_comment(self, comment):
+        self.driver.find_element(*self.comment_input).send_keys(comment)
+
+    def confirm_comment(self):
+        return self.driver.find_element(*self.confirm_comment_label).text
+
+    def toggle_blanket(self):
+        self.driver.find_element(*self.blanket_toggle).click()
+
+    def toggle_blanket_active(self):
+        return "active" in self.driver.find_element(*self.blanket_toggle).get_attribute("class")
+
+    def add_ice_cream(self):
+        self.driver.find_element(*self.ice_cream_button).click()
+
+    def order_ice_cream(self, quantity):
+        for _ in range(quantity):
+            self.add_ice_cream()
+
+    def ice_cream_quantity(self):
+        return self.driver.find_element(*self.ice_cream_quantity_label).text
+
+    def show_popup(self):
+        return self.driver.find_element(*self.confirmation_popup).text
